b3302cc1f8609f3315f5fcb9d5b276bf0055bb4d
16 <16@DESKTOP-LGL40OG>
2021-04-15 10:33:54 +0900

    third

--- src/boardclass/BoardClass.java
+++ src/boardclass/BoardClass.java
@@ -16,7 +16,7 @@ public class BoardClass {
 		this.bWriter = bWriter;
 		this.bContents = bContents;
 	}
-
+//모든 필드값을 출력
 	void boardview() {
 		System.out.println("----Construtor----");
 		System.out.println(bNumber);
@@ -24,7 +24,7 @@ public class BoardClass {
 		System.out.println(bWriter);
 		System.out.println(bContents);
 	}
-
+// 글 제목, 내용을 매개변수로 하여 필드값수정
 	void boardUpdate(String bTitle, String bContents) {
 		this.bContents = bContents;
 		this.bTitle = bTitle;
